ee38dddbe9ba69e0f04f63bd67ba8598d3772ba7
hasanalam24 <hasanalam365@gmail.com>
2024-05-11 02:29:17 +0600

    my request post delete route

--- index.js
+++ index.js
@@ -107,6 +107,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/request/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await requestCollection.deleteOne(query)
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
