12fa15bf0a39e75f00011702a2224e1fe0ed9599
hasanalam24 <hasanalam365@gmail.com>
2024-05-12 02:49:14 +0600

    add manage post

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors({
     origin: [
-        'http://localhost:5173', 'http://localhost:5174',
+        'http://localhost:5173', 'http://localhost:5174', "https://human-plannet-a11.web.app", "https://human-plannet-a11.firebaseapp.com"
     ],
     credentials: true
 }))
@@ -53,10 +53,17 @@ const verifyToken = (req, res, next) => {
 
 }
 
+const cookieOption = {
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    secure: process.env.NODE_ENV === "production" ? true : false
+
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const addPostCollection = client.db('VolunteerDB').collection('addPostDB')
@@ -67,20 +74,15 @@ async function run() {
         //auth related api
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            })
-                .send({ success: true })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '12h' })
+            res.cookie('token', token, cookieOption).send({ success: true })
 
         })
 
         app.post('/logout', async (req, res) => {
             const user = req.body;
             // console.log('login out user', user)
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+            res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ success: true })
         })
 
 
@@ -98,17 +100,25 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/addposts/:email', logger, verifyToken, async (req, res) => {
+        // app.get('/addposts/:email', logger, verifyToken, async (req, res) => {
 
+        //     const email = req.params.email
+
+        //     if (req.user.email !== email) {
+        //         return res.status(403).send({ message: 'fobidden access' })
+        //     }
+        //     const query = { email: email }
+        //     const result = await addPostCollection.find(query).toArray()
+        //     res.send(result)
+        // })
+
+        app.get('/mypost/:email', async (req, res) => {
             const email = req.params.email
-
-            if (req.user.email !== email) {
-                return res.status(403).send({ message: 'fobidden access' })
-            }
             const query = { email: email }
             const result = await addPostCollection.find(query).toArray()
             res.send(result)
         })
+
 
         app.post('/addpost', async (req, res) => {
             const addPost = req.body;
@@ -171,7 +181,7 @@ async function run() {
             res.send(result)
         })
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
